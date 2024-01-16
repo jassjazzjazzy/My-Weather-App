b6129d9bc3b28bb3cdfe7e142b69730793c23d3c
jassjazzjazzy <153749080+jassjazzjazzy@users.noreply.github.com>
2024-01-16 02:07:36 -0600

    place in weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ function UpdateWeatherData(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current
 let cityElement = document.querySelector("#weather-app-city");
+let descriptionElement = document.querySelector("#des");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
 
+timeElement.innerHTML = formatDate(date);
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windElement.innerHTML = `${response.data.wind.speed}m/h`;
 cityElement.innerHTML = response.data.city;
+descriptionElement.innerHTML= response.data.condition.description;
+ temperatureElement.innerHTML = Math.round(temperature);
 
-    temperatureElement.innerHTML = Math.round(temperature);
+}
 
+function formatDate(date) {
+
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ 
+   let day = days[date.getDay()]; 
+   if (minutes < 10) {
+    minutes = `0${minutes}`;
+   }
+
+   return  `${day} ${hours}:${minutes}`;
 }
 
 
