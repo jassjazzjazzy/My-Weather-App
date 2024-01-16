62d74ae22fb3be07d0edcaec7a8fb8c1cf5ee234
jassjazzjazzy <153749080+jassjazzjazzy@users.noreply.github.com>
2024-01-16 01:03:18 -0600

    updated the JS temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+function UpdateWeatherData(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current
+let cityElement = document.querySelector("#weather-app-city");
+
+cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+
+function searchCity(city) {
+let apiKey = "t6483ea502089504eb2ccb3fob3003f0";
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(UpdateWeatherData);
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#weather-app-city");
-cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Houston");
